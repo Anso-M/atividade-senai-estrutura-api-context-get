0e27b66e883fd3bc657c0ab4210fe78d8751cc63
Anderson Marcos <99432137+Anso-M@users.noreply.github.com>
2024-08-23 16:57:02 -0300

    criar login e CORS

--- obj/Debug/net6.0/Exo.WebApi.AssemblyInfo.cs
+++ obj/Debug/net6.0/Exo.WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1c480b79f9a1624bc2cfaca2b711269cf661d9c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b2d21fef3b4014aa1ad4166a2d1fd14618debc7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
